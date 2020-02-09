5caaf3674e68265ce9433679d292f84280785751
Kenny Mui <klmui@wisc.edu>
2020-02-09 15:08:43 -0600

    Add delete comment functionality

--- Routes/comments.js
+++ Routes/comments.js
@@ -69,6 +69,17 @@ router.put("/:comment_id", function(req, res){
     });
  });
 
+ // Comment destroy route
+ router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+ });
+
 // Middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
